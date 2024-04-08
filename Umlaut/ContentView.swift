de52c9113e7874d3a8cd28d017023d2ee5e29a16
Christian Grebe <christian.grebe@libellen-verlag.de>
2024-04-08 09:42:23 +0200

    Initial Commit

---
+++ Umlaut/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Umlaut
+//
+//  Created by Christian Grebe on 08.04.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Umlaut/UmlautApp.swift
@@ -0,0 +1,17 @@
+//
+//  UmlautApp.swift
+//  Umlaut
+//
+//  Created by Christian Grebe on 08.04.24.
+//
+
+import SwiftUI
+
+@main
+struct UmlautApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
